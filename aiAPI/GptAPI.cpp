126174a394c9252361c5acdc56c5d505a6004eba
Santiago Chaves <santichavesg@gmail.com>
2023-11-17 09:40:46 -0600

    GPT api posiblemente lista, erro en billing

--- aiAPI/GptAPI.cpp
+++ aiAPI/GptAPI.cpp
@@ -5,17 +5,17 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include "json.hpp"
+
 using namespace std;
 using json = nlohmann::json;
 
 #define CHUNK_SIZE 2048
-//sacado del repo del profe
+
 typedef struct
 {
     unsigned char *buffer;
     size_t len;
     size_t buflen;
-
 } get_request;
 
 // static method to act as callback for curl
@@ -39,29 +39,52 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
 };
 
 template <typename T>
-class apiGetter
+class GptAPI
 {
 private:
     CURL *curl;
     CURLcode res;
+    string apiKey;
 
 public:
-
-    T *getInfo(string urlToLook)
+    GptAPI(string apiKey)
     {
-        //dependiendo de la clase que se desee obtener se cambia el tipo de dato
-        T *result;
+        this->apiKey = apiKey;
+    }
+    T* askQuestion(string &question)
+    {
 
+        T* result;
         get_request req = {.buffer = NULL, .len = 0, .buflen = 0};
 
         curl_global_init(CURL_GLOBAL_ALL);
         curl = curl_easy_init();
 
         // build the URL with the proper entryid
-        string url = urlToLook;
+        string url = "https://api.openai.com/v1/chat/completions";
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+        struct curl_slist* headers = nullptr;
+        headers = curl_slist_append(headers, ("Authorization: Bearer " + apiKey).c_str());
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36");
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+
+        //request prompt
+        json payload = {
+            {"model", "gpt-3.5-turbo"},
+            {"messages", {
+                             {"role", "system"},
+                             {"content", question}
+                         }}};
+            
+        cout << payload.dump() << endl;
+        // Convert the JSON payload to a string
+        std::string payloadStr = payload.dump();
+
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payloadStr.c_str());
 
         // prepare the callback function to catch the result json
         req.buffer = (unsigned char *)malloc(CHUNK_SIZE);
@@ -77,27 +100,19 @@ public:
         if (res == CURLE_OK)
         {
             json responseData = json::parse(req.buffer);
-            /* cambia depende de que se desee obtener
-            for (auto &item : responseData["articles"])
-            {
-                try
-                {
-                    News *article = new News(item["author"].get<string>(),
-                                             item["title"].get<string>(),
-                                             item["description"].get<string>());
-
-                    result.push_back(article);
-                }
-                catch (const exception &e)
-                {
-                }
-            }
-            */
+            //result = new string(responseData["choices"]["message"]["content"].get<string>());
+            result = new string(responseData.dump());
             free(req.buffer);
         }
         else
         {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            if (req.buffer != nullptr)
+            {
+                json errorData = json::parse(req.buffer);
+                cout << "API Error: " << errorData["error"]["message"] << endl;
+                free(req.buffer);
+            }
         }
 
         // clean the curl objects
@@ -107,5 +122,17 @@ public:
         return result;
     };
 };
+
+int main(void)
+{
+    string* response = new string();
+    string question = "what is the meaning of life?";
+
+    GptAPI<string> gpt = GptAPI<string>("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");
+
+    response = gpt.askQuestion(question);
+    cout <<"API response: "<< *response << endl;
+    return 0;
+}
 
 #endif
