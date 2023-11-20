414a20d2642e5ff2dbd71aa050f7702009505348
Santiago Chaves <santichavesg@gmail.com>
2023-11-19 21:12:48 -0600

    rating method improved

--- prelim#2/ParagraphRating.h
+++ prelim#2/ParagraphRating.h
@@ -4,19 +4,21 @@
 #include <algorithm>
 #include <unordered_set>
 #include <climits>
+#include <sstream>
 
 using namespace std;
 
-int calculateRating(string paragraph,vector<string>words) {
-    unordered_set<string> wordSet(words.begin(), words.end());// que la busqueda sea O(1)
+int calculateRating(string paragraph, vector<string> words)
+{
+    unordered_set<string> wordSet(words.begin(), words.end()); // que la busqueda sea O(1)
     vector<int> positions;
-    istringstream parragraph(paragraph);//para recorrer por palabra
+    istringstream parragraph(paragraph); // para recorrer por palabra
     string word;
     int count = 0;
 
     while (parragraph >> word)
     {
-        //if(word.size() > 4)
+        // if(word.size() > 4)
         if (wordSet.find(word) != wordSet.end())
         {
             positions.push_back(count);
@@ -25,36 +27,31 @@ int calculateRating(string paragraph,vector<string>words) {
     }
 
     int rating = positions.size();
-    for(int i = 0; i < positions.size(); i++)
+    for (int i = 0; (i + 1) < positions.size(); i++)
     {
-        if((positions[i] - positions[i-1]) < 4)         //reglas
+        if ((positions[i + 1] - positions[i]) < 4) // reglas
         {
             rating += 4;
         }
-        else if((positions[i] - positions[i-1]) < 8)    //reglas
+        else if ((positions[i + 1] - positions[i]) < 8) // reglas
         {
             rating += 2;
         }
-        else                                            //reglas
+        else // reglas
         {
             rating += 1;
         }
     }
-    return rating > 1 ? rating : 1; //!esto es un poco feo, pero es que a veces da 0 porque en la lista sale 
-    }                               //!por ejemplo "the" y en el parrafo sale "the," entomces no lo asocia, entonces por lo menos asi lo medio contamos 
+    return rating > 1 ? rating : 1; //! esto es un poco feo, pero es que a veces da 0 porque en la lista sale
+} //! por ejemplo "the" y en el parrafo sale "the," entomces no lo asocia, entonces por lo menos asi lo medio contamos
 
-
-
-
-
-
-// int main() {
-//     string paragraph = "The beast of the fourth returns, as the beast arises , the night does so, the night is young beast beast beast beast night night hello hello hello beast night";
+// int main()
+// {
+//     string paragraph = "fourth ttatatat tatstst fourth fdfsff sd sfdff fs fsdf   sasdasd    attempt";
 //     vector<string> wordsToLookFor = {"fourth", "attempt", "beast"};
 
 //     int rating = calculateRating(paragraph, wordsToLookFor);
-//     int rate = calculateRating(paragraph, wordsToLookFor);
-//     std::cout << "Rating: " << rate << std::endl;
+//     std::cout << "Rating: " << rating << std::endl;
 
 //     return 0;
 // }
